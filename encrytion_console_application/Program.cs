7445f1e8ecf1eb228b06820d1850bf868067b604
Joseph Dagunan <jmax_dagunan@yahoo.com>
2016-02-27 01:18:47 -0500

    added base files -Joseph

---
+++ encrytion_console_application/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encrytion_console_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is a test - Joseph");
+        }
+    }
+}
