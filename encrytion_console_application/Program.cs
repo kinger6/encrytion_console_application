fad89911200ef507b796d4b02fb8e1fb61600a21
Joseph Dagunan <jmax_dagunan@yahoo.com>
2016-02-27 19:01:11 -0500

    created basic structure

--- encrytion_console_application/Program.cs
+++ encrytion_console_application/Program.cs
@@ -10,11 +10,49 @@ namespace CEC
     {
         public static void Main(string[] args)
         {
-            // this is how you would create a new program instance in a larger scale project.
-            CECApplication app = new CECApplication();
-            
+            // DECLARATION
+            string strUserInput;
+            bool ContinueApp = true;
+            bool TryAgain = true;
+
+           
+            // INPUT
+            Console.WriteLine("Encription Console Application");
+            Console.WriteLine("==============================");
+            Console.Write("Please enter a text to encrypt:");
 
 
+            // PROCCESS
+            do
+            {
+                // INPUT
+                Console.Clear();
+                Console.WriteLine("Encription Console Application");
+                Console.WriteLine("-------------------------------------");
+                Console.Write("Please enter a text to encrypt, type \"exit\" to close: ");
+
+                strUserInput = Console.ReadLine(); // Grabs data from user
+                
+                //Convertion Begins Here 
+                Console.WriteLine(strUserInput);
+
+                do
+                {
+                    Console.Write("Would you like to convert another string?(y/n): ");
+                    strUserInput = Console.ReadLine();
+                    if (strUserInput == "y") {
+                        TryAgain = false;
+                    }
+                    else if (strUserInput == "n")
+                    { 
+                        ContinueApp = false;
+                    }
+                } while (TryAgain);
+
+
+            }while(ContinueApp);
+
+            Environment.Exit(0);
         }
     }
 }
